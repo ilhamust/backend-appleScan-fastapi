f92c5f403934efdfe7187ba7748beb54838fa84c
ilhamust <ilhammstqm8@gmail.com>
2026-02-04 10:51:18 +0700

    Update CORS: add apple-scan-react.vercel.app

--- app/main.py
+++ app/main.py
@@ -21,7 +21,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "http://localhost:3000",
-        "https://frontend-apple-scan-react.vercel.app"
+        "https://frontend-apple-scan-react.vercel.app",
+        "https://apple-scan-react.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
